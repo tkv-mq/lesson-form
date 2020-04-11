b745deacfeae69afdcc9901b6490553168c80e52
Mq <toktorovm@gmail.com>
2020-04-11 11:54:00 +0600

    lesson-forms

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    note = open("note.txt", "r", encoding="utf-8")
+    note_lst = [i for i in note]
+    note.close()
+    return render_template('home.html', note_lst=note_lst)
+
+@app.route("/hello/<string:user_name>")
+def hello(user_name):
+    user_name = user_name.capitalize()
+    return render_template("hello.html", user_name=user_name)
+
+@app.route("/add-note-form")
+def add_note_form():
+    return render_template("add_form.html")
+
+@app.route("/add-note", methods=["POST"])
+def add_note():
+    note = request.form["new-note"]
+    note_file = open("note.txt", "a+", encoding="utf-8")
+    note_file.write(note + "\n")
+    note_file.close()
+    return render_template("ok.html")
